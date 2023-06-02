05991a41209f139e8c9be752d5a8ef47099e8cc8
Restyled.io <commits@restyled.io>
2023-06-02 12:43:48 +0000

    Restyled by pg_format

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,35 +1,29 @@
-
 BEGIN;
 DROP TABLE IF EXISTS eventTags;
 DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS tags;
-
 CREATE TABLE tags (
-    tag_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    tag_name VARCHAR(255) NOT NULL
+    tag_id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+    tag_name varchar(255) NOT NULL
 );
-
-
-
 CREATE TABLE events (
-    event_id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    thumbnail VARCHAR(255),
-    society VARCHAR(255),
-    location VARCHAR(255),
-    latitude NUMERIC(9, 6),
-    longitude NUMERIC(9, 6),
-    start_datetime TIMESTAMP,
-    ticket_price NUMERIC(10, 2)
+    event_id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+    name varchar(255) NOT NULL,
+    description text,
+    thumbnail varchar(255),
+    society varchar(255),
+    location varchar(255),
+    latitude numeric(9, 6),
+    longitude numeric(9, 6),
+    start_datetime timestamp,
+    ticket_price numeric(10, 2)
 );
-
 CREATE TABLE eventTags (
-    event_tag_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    event_id UUID,
-    tag_id UUID,
+    event_tag_id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+    event_id uuid,
+    tag_id uuid,
     FOREIGN KEY (event_id) REFERENCES events (event_id),
     FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
 );
-
 COMMIT;
+
