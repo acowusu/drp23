b972ee76294f3df817f0b8cfdc65dae2b4f148cd
Joel Ulens <joelulens@dyn3186-8.wlan.ic.ac.uk>
2023-06-08 12:02:21 +0100

    Forgot to add myeventsvue file to git

---
+++ src/views/MyEventsView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="results">
+      <EventCard v-for="item in items" :key="item" :data="item" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import EventCard from "@/components/EventCard.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "MyEventsView",
+  components: {
+    EventCard,
+  },
+  async mounted() {
+    for (let i = 0; i < localStorage.length; i++) {
+      const event_id = localStorage.key(i);
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ event_id }),
+      };
+      await fetch("/api/event", options)
+        .then((response) => response.json())
+        .then((response) => this.items.push(response));
+    }
+    console.log(localStorage.length);
+    console.log(localStorage.key(0));
+  },
+  data() {
+    return {
+      items: [] as Array<any>,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.results {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
